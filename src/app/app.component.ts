be2490f621e9874d9da3617e1e6192403eb163ba
noor <noor.khraisat@inspirejo.com>
2022-09-28 10:29:10 +0300

    --

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,85 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'lozana-farm-form';
+  slides = [
+    { 'image': 'https://static.remove.bg/remove-bg-web/3ad3b721d276f1af1fb7121aff638a866139749a/assets/start-1abfb4fe2980eabfbbaaa4365a0692539f7cd2725f324f904565a9a744f8e214.jpg' },
+    { 'image': 'https://static.remove.bg/remove-bg-web/3ad3b721d276f1af1fb7121aff638a866139749a/assets/start-1abfb4fe2980eabfbbaaa4365a0692539f7cd2725f324f904565a9a744f8e214.jpg' },
+    { 'image': 'https://static.remove.bg/remove-bg-web/3ad3b721d276f1af1fb7121aff638a866139749a/assets/start-1abfb4fe2980eabfbbaaa4365a0692539f7cd2725f324f904565a9a744f8e214.jpg' },
+    { 'image': 'https://static.remove.bg/remove-bg-web/3ad3b721d276f1af1fb7121aff638a866139749a/assets/start-1abfb4fe2980eabfbbaaa4365a0692539f7cd2725f324f904565a9a744f8e214.jpg' },
+    { 'image': 'https://static.remove.bg/remove-bg-web/3ad3b721d276f1af1fb7121aff638a866139749a/assets/start-1abfb4fe2980eabfbbaaa4365a0692539f7cd2725f324f904565a9a744f8e214.jpg' }
+  ];
+  title = 'Angular Reactive Form Validation';
+  submittedRequest: any;
+  angForm: FormGroup = new FormGroup({
+    name: new FormControl(''),
+    joinUs: new FormControl(''),
+    email: new FormControl(''),
+    phoneNumber: new FormControl(''),
+    adress: new FormControl(''),
+    haveCar: new FormControl(''),
+    pickupOthers: new FormControl(''),
+    howMany: new FormControl(''),
+    transportationMethod: new FormControl(''),
+
+
+
+
+  });
+  constructor(private fb: FormBuilder, private httpClient: HttpClient) {
+    this.createForm();
+    this.submittedRequest = localStorage.getItem("submittedRequest");
+  }
+  createForm() {
+    this.angForm = this.fb.group({
+      name: ['', Validators.required],
+      joinUs: ['', Validators.required],
+      email: ['', Validators.required],
+      phoneNumber: ['', Validators.required],
+      adress: ['', Validators.required],
+      haveCar: ['', Validators.required],
+      pickupOthers: ['', Validators.required],
+      howMany: ['', Validators.required],
+      transportationMethod: ['', Validators.required],
+
+
+    });
+  }
+  logData() {
+    if (!this.angForm.value.name) {
+      return
+    }
+    if (!this.angForm.value.joinUs) {
+      return
+    }
+    if (this.angForm.value.joinUs == 'true' && !this.angForm.value.email) {
+      return
+    }
+    if (this.angForm.value.joinUs == 'true' && !this.angForm.value.phoneNumber) {
+      return
+    }
+    if (this.angForm.value.joinUs == 'true' && !this.angForm.value.adress) {
+      return
+    }
+    if (this.angForm.value.joinUs == 'true' && !this.angForm.value.transportationMethod) {
+      return
+    }
+    if (this.angForm.value.joinUs == 'true' && !this.angForm.value.transportationMethod) {
+      return
+    }
+    this.httpClient.post(
+      'https://lozan-farm-default-rtdb.firebaseio.com/users.json',
+      this.angForm.value
+    ).subscribe(r => {
+      localStorage.setItem('submittedRequest', this.angForm.value)
+      this.submittedRequest = this.angForm.value
+    }
+    )
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatButtonModule} from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { MatCarouselModule } from 'ng-mat-carousel';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatRadioModule,
+    MatInputModule,
+    MatGridListModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatCarouselModule.forRoot()
+
   ],
   providers: [],
   bootstrap: [AppComponent]
